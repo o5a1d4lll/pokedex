1f41b4241653bc935c6ed837c3b4769a521085e1
o5a1d4lll <luizgustavo.cedesp@gmail.com>
2024-06-11 13:55:45 -0300

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,31 @@
+const caixaResultado = document.getElementById("resultado");
+
+function insert(num){
+
+caixaResultado.innerHTML += num;
+    
+}
+
+function clean(){
+
+caixaResultado.innerHTML = "";
+
+}
+
+function back(){
+
+caixaResultado.innerHTML;
+
+let resultadoFinal = caixaResultado.innerHTML;
+
+caixaResultado.innerHTML = resultadoFinal.substring(0,resultadoFinal.length-1) 
+
+}
+
+function calcular(){
+
+let calcular = caixaResultado.innerHTML; 
+
+caixaResultado.innerHTML = eval(resultado);
+
+}
